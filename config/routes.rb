69148d8e9638466a81210ed9fb4d55cf1d844256
NguyenDucChinhENC <Nguyenducchinh.hust@gmail.com>
2017-05-16 19:10:38 +0700

    add about help

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "static_pages#home", page: "home"
   get "/static_pages/:page", to: "static_pages#show"
+  get "/about", to: "static_pages#about"
+  get "/help", to: "static_pages#help"
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
