840e705c64c30e3bd4c093c188d9bd054b2ba4a2
Hoang Anh Tu <hoangtuyb96@gmail.com>
2017-05-16 18:21:18 +0700

    advanced_login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessor :remember_token
 
   has_many :comment, dependent: :destroy
   has_many :orders, dependent: :destroy
@@ -19,6 +20,22 @@ class User < ApplicationRecord
         BCrypt::Engine.cost
       BCrypt::Password.create string, cost: cost
     end
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+    
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password?remember_token
+  end
+
+  def forget 
+    update_attributes remember_digest: nil
   end
 
   private
